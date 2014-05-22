70644697f9ab8711cdc8c111452c9d1bb7bb16f8
Valentin Radulescu <valentin.radulescu@ubervu.com>
2014-05-22 13:39:04 +0300

    add basic GitHub functionality #2

--- js/changelog.js
+++ js/changelog.js
@@ -1,33 +1,46 @@
-(function($){
+;(function($, window, document, undefined){
 
-  var Changelog = function(target, options){
-
-    // Defaults
-    this._options = $.extend(
-      {
-        reloadText: 'Reload',
-        iconText: 'Link',
-        footerTitle: 'Changelog'
-      }, options
-    );
-
-    this._widget = null;
-    this._list = null;
-    this._listWrapper = null;
-    this._reloadButton = null;
-    this._mainButton = null;
-    this._currentTimestamp = Date.now();
-    this._target = target;
-    this._init(target, options);
-    return this;
+  var defaults = {
+    reloadText: 'Reload',
+    iconText: 'Link',
+    footerTitle: 'Changelog',
+    owner: 'facebook',
+    repo: 'react',
+    state: 'closed',
+    labels: 'build/tooling',
+    sort: 'updated'
   };
 
-  Changelog.prototype._updateTimestamp = function(){
-    this._currentTimestamp = Date.now();
+  var Changelog = function(element, options){
+    this.element = element;
+    this.$element = $(element);
+    this.options = $.extend({}, defaults, options)
+
+    //GitHub
+    this.since = Date.now();
+    this.apiBase = 'https://api.github.com';
+    this.endPoint = this.apiBase + '/repos/' + this.options.owner + '/' + this.options.repo + '/issues';
   };
 
-  Changelog.prototype._addListItem = function(title, label, labelColor){
-    this._list.append(
+  Changelog.prototype.checkForUpdates = function(){
+    var options = {
+      state: this.options.state,
+      labels: this.options.labels,
+      sort: this.options.sort,
+      since: new Date(2014, 0)
+    };
+    var self = this;
+    $.get(self.endPoint, options)
+      .done(function(data){
+        if(data.length !== 0){
+          self.buildList(data.length);
+        }
+      });
+  }
+
+  Changelog.prototype.addListItem = function(title, label, labelColor){
+    var list = this.$element.find('ul');
+    list.append(
       $('<li></li>')
         .append(
           $('<span class="label"></span>')
@@ -39,59 +52,47 @@
         )
     );
   };
-
-  Changelog.prototype._buildList = function(){
-    var iconText = this._options.iconText;
-    this._widget = 
+  Changelog.prototype.buildList = function(notificationNum){
+    var widget = 
       $('<div class="changelog-wrapper bottom"></div>')
         .append(
           $('<a class="btn" href="#"></a>')
-            .text(this._options.iconText)
+            .text(this.options.iconText)
             .append($('<span class="badge"></span>')
-              .text(22)
+              .text(notificationNum)
             )
           )
         .append(
           $('<div class="changelog"></div>')
             .append($('<ul></ul>'))
             .append($('<div class="changelog-footer"></div>')
-              .text(this._options.footerTitle + ' (' + 22 + ')')
+              .text(this.options.footerTitle + ' (' + notificationNum + ')')
               .append($('<a class="btn" href="#"></a>')
-                .text(this._options.reloadText)
+                .text(this.options.reloadText)
               )
             )
-        );
-    this._list = this._widget.find('ul');
-    this._listWrapper = this._widget.find('.changelog');
-    this._reloadButton = this._widget.find('.changelog-footer .btn');
-    this._mainButton = this._widget.children('.btn').first();
-    
+        );    
     // add widget to selector
-    this._target.html(' ').append(this._widget);
-  };
-
-  Changelog.prototype._init = function(){
-    this._buildList();
-  };
-
-  Changelog.prototype.checkForUpdates = function(){
+    this.$element.html(' ').append(widget);
   };
 
   $.fn.changelog = function(options){
+    
+    var args = arguments;
+    
+    return this.each(function(){
+      var instance = $(this).data('changelog');
 
-    var instance = this.data('changelog');
+      if(instance && instance[options]){
+        return instance[options].apply(instance, Array.prototype.slice.call(args, 1));
 
-    if(instance && instance[options]){
-      return instance[options].apply(instance, Array.prototype.slice.call(arguments, 1));
-
-    }else if(typeof options === 'object' || !options){
-      instance = new Changelog(this, options);
-      this.data('changelog', instance );
-      return this;
-
-    }else{
-      $.error('Method ' +  options + ' does not exist');
-    }    
+      }else if(typeof options === 'object' || !options){
+        instance = new Changelog(this, options);
+        $(this).data('changelog', instance );
+      }else{
+        $.error('Method ' +  options + ' does not exist');
+      }
+    });    
   };
 
-})(jQuery);
+})(jQuery, window, document);
